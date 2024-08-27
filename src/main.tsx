fc98ec00427f6ebe3c9eed8b776cc5fad290e81f
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-08-27 17:01:24 +0100

    Remove unauthorized page

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/default/HomePage/HomePage'
 import NotFoundPage from './pages/default/NotFoundPage/NotFoundPage'
 import PageContainer from './components/containers/PageContainer/PageContainer'
-import UnauthorizedPage from './pages/default/UnauthorizedPage/UnauthorizedPage'
 import LogoutPage from './pages/default/LogoutPage/LogoutPage'
 import ProtectedRoute from './utils/ProtectedRoute/ProtectedRoute'
 import { AuthRole } from './context/AuthContext/IAuthContext'
@@ -17,7 +16,6 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={ <PageContainer> <HomePage /> </PageContainer> } />
-          <Route path={'/unauthorized'} element={<PageContainer> <UnauthorizedPage/> </PageContainer>}/>
           <Route path={'/logout'} element={<PageContainer> <LogoutPage/> </PageContainer>}/>
           <Route element={ <ProtectedRoute roles={[AuthRole.ADMIN]}/> }>
               <Route path={'/test'} element={<PageContainer> <HomePage/> </PageContainer>}/>
