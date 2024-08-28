ee0c65b53f3434a8f3b6ed2dfd8fd540ecd65762
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-08-28 15:19:41 +0100

    Fix weights

--- src/pages/default/SignInPage/SignInPage.tsx
+++ src/pages/default/SignInPage/SignInPage.tsx
@@ -4,13 +4,19 @@ import { IButtonColor } from '../../../components/CustomButton/ICustomButton'
 import InputField from '../../../components/InputField/InputField'
 import './SignInPage.css'
 import { FaLock, FaRegUser } from 'react-icons/fa'
+import { useRef, useState } from 'react'
 
 export default function SignInPage() {
+
+    const identifier = useRef<string>("");
+    const password = useRef<string>("");
+    const [dummy, setDummy] = useState<string>("");
+
     return (
         <div className='sign-in-page'>
             <span className='title'>Welcome to PixelJudge</span>
-            <InputField icon={ <FaRegUser /> } placeholder='Username' />
-            <InputField icon={ <FaLock /> } placeholder='Password' type='password' />
+            <InputField value={ identifier } error={ dummy } setError={ setDummy } icon={ <FaRegUser /> } placeholder='Username' />
+            <InputField value={ password } error={ dummy } setError={ setDummy } icon={ <FaLock /> } placeholder='Password' type='password' />
             <CustomButton color={ IButtonColor.ORANGE } text='Sign In' onClick={ () => {} } />
             <span>Don't have an account? <Link to='/sign-up'>Sign up now</Link></span>
         </div>
