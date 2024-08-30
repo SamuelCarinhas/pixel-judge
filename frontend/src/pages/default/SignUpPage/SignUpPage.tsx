53ec9bf90a7822d6d13bd277fe9aaca4849fe089
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-08-30 15:31:50 +0100

    Add sign up integration

--- frontend/src/pages/default/SignUpPage/SignUpPage.tsx
+++ frontend/src/pages/default/SignUpPage/SignUpPage.tsx
@@ -7,6 +7,10 @@ import { FaLock, FaRegUser } from 'react-icons/fa'
 import { MdOutlineMailOutline } from 'react-icons/md'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import Loading from '../../../components/Loading/Loading'
+import { useState } from 'react'
+import axios from 'axios'
+
+const REST_URL = import.meta.env.VITE_REST_URL
 
 type SignUpInput = {
     username: string
@@ -24,16 +28,43 @@ export default function SignUpPage() {
         formState: { errors, isSubmitting }
     } = useForm<SignUpInput>();
 
+    const [completed, setCompleted] = useState<boolean>(false);
+
 
     const onSubmit: SubmitHandler<SignUpInput> = async (data) => {
-        await new Promise((res) => setTimeout(res, 1000));
-        console.log(data);
-        setError('root', {
-            message: "Server error"
-        })
+        if(data.confirmPassword !== data.password) {
+            setError("confirmPassword", {
+                message: "The passwords are not equal"
+            })
+            return;
+        }
+        try {
+            await axios.post(`${REST_URL}/auth/sign-up`, data);
+            setCompleted(true);
+        } catch(error) {
+            if(!axios.isAxiosError(error)) {
+                setError("root", { message: "This was not supposed to happen."});
+                return;
+            }
+            
+            if(error.response && error.response.data && error.response.data.description) {
+                const errors = error.response.data.description;
+                for (let [key, value] of Object.entries(errors)) {
+                    setError(key as keyof SignUpInput, { message: value as string });
+                }
+            } else {
+                setError("root", { message: "The server failed to respond" });
+            }
+            return;
+        }
     }
 
     return (
+        completed ?
+        <div className='sign-up-page'>
+            <span className='sign-up-success'>A confirmation email has been sent to your inbox. Please follow the instructions provided to complete the process.</span>
+        </div>
+        :
         <form className='sign-up-page' onSubmit={ handleSubmit(onSubmit) }>
             <span className='title'>Welcome to PixelJudge</span>
             <span>Create a new account</span>
