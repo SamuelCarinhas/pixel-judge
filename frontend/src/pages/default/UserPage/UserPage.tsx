cbca93beffb85de5fd4717c6660c04e0a9fc1202
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-09-02 12:03:54 +0100

    Add user page

--- frontend/src/pages/default/UserPage/UserPage.tsx
+++ frontend/src/pages/default/UserPage/UserPage.tsx
@@ -1,13 +1,67 @@
 import { useParams } from 'react-router-dom'
 import './UserPage.css'
+import { useEffect, useState } from 'react';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import axios from 'axios';
+import { IProfile } from '../../../utils/models/profile.model';
+
+const REST_URL = import.meta.env.VITE_REST_URL
 
 export default function UserPage() {
 
     const { username } = useParams();
+    const [notFound, setNotFound] = useState<boolean>(false);
+    const [profile, setProfile] = useState<IProfile>({
+        firstName: null,
+        secondName: null,
+        birthDate: null,
+        country: null,
+        city: null,
+        organization: null,
+        lastVisit: null,
+        registered: new Date()
+    });
+
+    function updateProfile(profile: IProfile) {
+        if(profile.birthDate !== null) profile.birthDate = new Date(profile.birthDate);
+        if(profile.lastVisit !== null) profile.lastVisit = new Date(profile.lastVisit);
+        profile.registered = new Date(profile.registered);
+        setProfile(profile);
+    }
+
+    useEffect(() => {
+        if(!username) setNotFound(true);
+
+        axios.get(`${REST_URL}/profile?username=${username}`)
+        .then(res => updateProfile(res.data.profile))
+        .catch(() => setNotFound(true));
+    }, [username]);
 
     return (
-        <div>
-            { username }
+        notFound ?
+        <NotFoundPage />
+        :
+        <div className='user-page'>
+            <div className='user-info'>
+                <div className='user'>
+                    <img src={"https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"}/>
+                    <div className='user-id'>
+                        <span className='rank'>Administrator</span>
+                        <span className='username'>@carinhas</span>
+                        <span className='full-name'>Samuel Carinhas</span>
+                    </div>
+                </div>
+                <div className='information'>
+                    <h3>Information</h3>
+                    <span>University</span>
+                    <span>Location</span>
+                </div>
+                <div className='badges'>
+                    <h3>Badges</h3>
+                </div>
+            </div>
+            <div className='user-activity'>
+            </div>
         </div>
     )
 }
---
+++ frontend/src/utils/models/profile.model.ts
@@ -0,0 +1,10 @@
+export interface IProfile {
+    firstName: string | null,
+    secondName: string | null,
+    birthDate: Date | null,
+    country: string | null,
+    city: string | null,
+    organization: string | null,
+    lastVisit: Date | null,
+    registered: Date
+}
