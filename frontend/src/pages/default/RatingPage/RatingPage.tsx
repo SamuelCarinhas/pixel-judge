160684b15df97f1d2c03381488f24fe410005a8e
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-09-03 12:00:40 +0100

    Add rating page

--- frontend/src/pages/default/RatingPage/RatingPage.tsx
+++ frontend/src/pages/default/RatingPage/RatingPage.tsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from 'react';
 import './RatingPage.css'
-import constructing from '../../../assets/gifs/construction.gif'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const REST_URL = import.meta.env.VITE_REST_URL
 
 export default function RatingPage() {
 
+    const [users, setUsers] = useState<{username: string}[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`${REST_URL}/profile/all`)
+        .then(res => {
+            setUsers(res.data.profiles);
+        })
+        .catch(() => {});
+    })
+
     return (
         <div className='rating-page'>
-            <span>We are currently building this page</span>
-            <img src={constructing}></img>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Username</th>
+                        <th>Rating</th>
+                    </tr>
+                    {
+                        users.map((user, key) => (
+                            <tr key={key}>
+                                <th className='rank'>{key+1}</th>
+                                <th className='username' onClick={ () => navigate(`/user/${user.username}`)}>@{user.username}</th>
+                                <th className='rating'>0</th>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
