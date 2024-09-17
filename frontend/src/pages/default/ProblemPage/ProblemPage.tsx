f7aacae7bcf93cd416ebd2f2db1c5cf1425b82bd
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-09-17 15:23:26 +0100

    Improve problem page

--- frontend/src/pages/default/ProblemPage/ProblemPage.tsx
+++ frontend/src/pages/default/ProblemPage/ProblemPage.tsx
@@ -1,22 +1,59 @@
 import { useParams } from 'react-router-dom'
 import './ProblemPage.css'
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { IAdminProblem } from '../../../utils/models/admin.model';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import axiosInstance from '../../../utils/axios';
 import MarkdownContainer from '../../../components/containers/MarkdownContainer/MarkdownContainer';
+import { CiTimer } from 'react-icons/ci';
+import { IoIosPricetags, IoIosSend } from 'react-icons/io';
+import { GoGraph } from 'react-icons/go';
+import { AuthContext } from '../../../context/AuthContext/AuthContext';
+import { AuthRole } from '../../../context/AuthContext/IAuthContext';
+import InputFile from '../../../components/InputFile/InputFile';
+import { FaUpload } from 'react-icons/fa';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import CustomButton from '../../../components/CustomButton/CustomButton';
+import { IButtonColor } from '../../../components/CustomButton/ICustomButton';
+import Loading from '../../../components/Loading/Loading';
+import { AlertContext } from '../../../context/AlertContext/AlertContext';
+import { AlertType } from '../../../context/AlertContext/IAlertContext';
+
+type SubmitInput = {
+    code: FileList
+}
 
 export default function ProblemPage() {
 
     const { id } = useParams();
     const [notFound, setNotFound] = useState(false);
     const [problem, setProblem] = useState<IAdminProblem>();
+    const { role } = useContext(AuthContext);
+    const { addAlert } = useContext(AlertContext);
 
     useEffect(() => {
         axiosInstance.get(`/problem?id=${id}`)
             .then((res) => setProblem(res.data.problem))
             .catch(() => setNotFound(true));
     }, [id]);
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isSubmitting }
+    } = useForm<SubmitInput>();
+
+    const onSubmit: SubmitHandler<SubmitInput> = async (data) => {
+        console.log(data);
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        setError('root', { message: "Server error" });
+        addAlert({
+            type: AlertType.INFO,
+            title: 'Info',
+            text: 'Solution submitted'
+        })
+    }
 
     return (
         notFound ? <NotFoundPage />
@@ -66,7 +103,60 @@ export default function ProblemPage() {
                 }
             </div>
             <div className='side-bar'>
-                This will be a side bar
+                {
+                    role !== AuthRole.LOADING && role !== AuthRole.DEFAULT
+                    &&
+                    <>
+                        <div className='side-option submit-problem'>
+                            <div className='header'><CiTimer /> Recent Submissions</div>
+                            <div className='content'>
+                                None
+                            </div>
+                        </div>
+                        <div className='side-option submit-problem'>
+                            <div className='header'><IoIosSend /> Submit Problem</div>
+                            <div className='content'>
+                                <form onSubmit={ handleSubmit(onSubmit) }>
+                                {
+                                    isSubmitting ?
+                                    <Loading />
+                                    :
+                                    <>
+                                        <InputFile
+                                            icon={<FaUpload />}
+                                            description='Choose file'
+                                            label='code'
+                                            error={errors.code}
+                                            {...register('code',
+                                                {
+                                                    required: 'File is required',
+                                                    validate: {
+                                                        singleFile: (files) => files.length === 1 || 'Please select only one file'
+                                                    }
+                                                }
+                                            )}
+                                        />
+                                        <CustomButton disabled={ isSubmitting } type='submit' color={ IButtonColor.GREEN } text='Submit' />
+                                        { errors.root && <span className='red'>{ errors.root.message }</span> }
+                                    </>
+                                }
+                                </form>
+                                </div>
+                        </div>
+                    </>
+                }
+                <div className='side-option submit-problem'>
+                    <div className='header'><IoIosPricetags /> Tags</div>
+                    <div className='content'>
+                        None
+                    </div>
+                </div>
+                <div className='side-option submit-problem'>
+                    <div className='header'><GoGraph /> Statistics</div>
+                    <div className='content'>
+                        None
+                    </div>
+                </div>
             </div>
         </div>
     )
