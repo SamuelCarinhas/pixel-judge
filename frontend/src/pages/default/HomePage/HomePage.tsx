0f02f7c025253caf3517fa915b0d8c19635389eb
Samuel Carinhas <samuelsantos.c.2001@gmail.com>
2024-09-20 17:09:15 +0100

    Add Home page building gif

--- frontend/src/pages/default/HomePage/HomePage.tsx
+++ frontend/src/pages/default/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
-import Loading from '../../../components/Loading/Loading'
 import './HomePage.css'
+import constructing from '../../../assets/gifs/construction.gif'
 
 export default function HomePage() {
 
     return (
         <div className="home-page">
-            <Loading />
+            This page is not ready yet, please go explore other ones
+            <span>We are currently building this page</span>
+            <img src={constructing}></img>
         </div>
     )
 }
